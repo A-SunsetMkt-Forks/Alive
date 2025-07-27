d749711b61a903f5e951cc01d360eedd88d850f6
TopSea <cgbdyw@gmail.com>
2025-07-27 20:14:14 +0800

    更新文档。

---
+++ src/components/vmdConverterScene.ts
@@ -0,0 +1,122 @@
+import { FreeCamera } from "@babylonjs/core/Cameras/freeCamera";
+import type { AbstractEngine } from "@babylonjs/core/Engines/abstractEngine";
+import { Vector3 } from "@babylonjs/core/Maths/math.vector";
+import { Scene } from "@babylonjs/core/scene";
+import type { ISceneBuilder } from "../baseRuntime";
+import { VmdLoader } from "babylon-mmd/esm/Loader/vmdLoader";
+import { BvmdConverter } from "babylon-mmd/esm/Loader/Optimized/bvmdConverter";
+
+
+export class SceneBuilder implements ISceneBuilder {
+    public async build(canvas: HTMLCanvasElement, engine: AbstractEngine): Promise<Scene> {
+        engine.setHardwareScalingLevel(1000);
+        const scene = new Scene(engine);
+        new FreeCamera("camera1", new Vector3(0, 5, -10), scene);
+
+        const formDiv = document.createElement("div");
+        formDiv.style.position = "absolute";
+        formDiv.style.top = "0";
+        formDiv.style.left = "0";
+        formDiv.style.backgroundColor = "white";
+        formDiv.style.width = "100%";
+        formDiv.style.height = "100%";
+        formDiv.style.display = "flex";
+        formDiv.style.flexDirection = "column";
+        formDiv.style.justifyContent = "center";
+        formDiv.style.alignItems = "center";
+        formDiv.style.fontFamily = "sans-serif";
+        canvas.parentElement!.appendChild(formDiv);
+
+        const innerFormDiv = document.createElement("div");
+        innerFormDiv.style.width = "auto";
+        innerFormDiv.style.height = "100%";
+        innerFormDiv.style.display = "flex";
+        innerFormDiv.style.flexDirection = "column";
+        innerFormDiv.style.justifyContent = "center";
+        innerFormDiv.style.alignItems = "start";
+        formDiv.appendChild(innerFormDiv);
+
+        const vmdLoader = new VmdLoader(scene);
+        vmdLoader.loggingEnabled = true;
+
+        const files: File[] = [];
+
+        const title = document.createElement("h1");
+        title.textContent = "VMD to BVMD Converter";
+        title.style.width = "350px";
+        title.style.fontSize = "24px";
+        title.style.textAlign = "center";
+        innerFormDiv.appendChild(title);
+
+        const loadedFileList = document.createElement("ul");
+        innerFormDiv.appendChild(loadedFileList);
+        loadedFileList.style.width = "100%";
+        loadedFileList.style.height = "auto";
+        loadedFileList.style.fontSize = "20px";
+
+        const renderLoadedFiles = (): void => {
+            loadedFileList.innerHTML = "";
+            for (const file of files) {
+                const item = document.createElement("li");
+                item.textContent = file.name;
+                loadedFileList.appendChild(item);
+            }
+        };
+        renderLoadedFiles();
+
+        const fileInput = document.createElement("input");
+        fileInput.style.width = "100%";
+        fileInput.style.height = "80px";
+        fileInput.style.display = "block";
+        fileInput.style.backgroundColor = "black";
+        fileInput.style.color = "white";
+        fileInput.style.marginBottom = "10px";
+        fileInput.style.fontSize = "20px";
+
+        fileInput.type = "file";
+        fileInput.accept = ".vmd";
+        fileInput.multiple = true;
+        fileInput.onchange = (): void => {
+            if (fileInput.files === null) return;
+            for (const file of fileInput.files) {
+                if (!file.name.endsWith(".vmd")) continue;
+                files.push(file);
+            }
+            renderLoadedFiles();
+        };
+        innerFormDiv.appendChild(fileInput);
+
+        const convertButton = document.createElement("button");
+        convertButton.textContent = "Convert";
+        convertButton.style.width = "100%";
+        convertButton.style.height = "60px";
+        convertButton.style.border = "none";
+        convertButton.style.fontSize = "20px";
+
+        convertButton.onclick = async(): Promise<void> => {
+            if (files.length === 0) {
+                alert("Please select a VMD file");
+                return;
+            }
+
+            convertButton.textContent = "Converting...";
+            const animation = await vmdLoader.loadAsync(files[0].name, files);
+            const arrayBuffer = BvmdConverter.Convert(animation);
+            const blob = new Blob([arrayBuffer], { type: "application/octet-stream" });
+            const url = URL.createObjectURL(blob);
+            const a = document.createElement("a");
+            a.href = url;
+            a.download = `${files[0].name.substring(0, files[0].name.lastIndexOf("."))}.bvmd`;
+            a.click();
+            URL.revokeObjectURL(url);
+            a.remove();
+
+            files.length = 0;
+            renderLoadedFiles();
+            convertButton.textContent = "Convert";
+        };
+        innerFormDiv.appendChild(convertButton);
+
+        return scene;
+    }
+}
---
+++ src/pages/vmd_converter.vue
@@ -0,0 +1,62 @@
+<script setup lang="ts">
+import "@babylonjs/core/Engines/WebGPU/Extensions/engine.alpha";
+import "@babylonjs/core/Engines/WebGPU/Extensions/engine.multiRender";
+import "@babylonjs/core/Engines/WebGPU/Extensions/engine.rawTexture";
+import "@babylonjs/core/Engines/WebGPU/Extensions/engine.readTexture";
+import "@babylonjs/core/Engines/WebGPU/Extensions/engine.renderTarget";
+import "@babylonjs/core/Engines/WebGPU/Extensions/engine.renderTargetTexture";
+
+import { BaseRuntime } from "../baseRuntime";
+import { SceneBuilder } from "../components/vmdConverterScene";
+
+const canvas = document.createElement("canvas");
+canvas.style.width = "100%";
+canvas.style.height = "100%";
+canvas.style.display = "block";
+document.body.appendChild(canvas);
+
+let engine;
+
+const useWebGPU = false;
+if (useWebGPU) {
+    engine = new (await import("@babylonjs/core/Engines/webgpuEngine")).WebGPUEngine(canvas, {
+        stencil: false,
+        antialias: true,
+        doNotHandleTouchAction: true,
+        doNotHandleContextLost: true,
+        audioEngine: false,
+        glslangOptions: {
+            jsPath: new URL("@babylonjs/core/assets/glslang/glslang.js", import.meta.url).href,
+            wasmPath: new URL("@babylonjs/core/assets/glslang/glslang.wasm", import.meta.url).href
+        },
+        twgslOptions: {
+            jsPath: new URL("@babylonjs/core/assets/twgsl/twgsl.js", import.meta.url).href,
+            wasmPath: new URL("@babylonjs/core/assets/twgsl/twgsl.wasm", import.meta.url).href
+        }
+    });
+    await engine.initAsync();
+} else {
+    engine = new (await import("@babylonjs/core/Engines/engine")).Engine(canvas, false, {
+        preserveDrawingBuffer: false,
+        stencil: false,
+        antialias: true,
+        alpha: true,
+        premultipliedAlpha: false,
+        powerPreference: "high-performance",
+        doNotHandleTouchAction: true,
+        doNotHandleContextLost: true,
+        audioEngine: false,
+        disableWebGL2Support: false
+    }, true);
+}
+
+BaseRuntime.Create({
+    canvas,
+    engine,
+    sceneBuilder: new SceneBuilder()
+}).then(runtime => runtime.run());
+</script>
+
+<template>
+
+</template>
